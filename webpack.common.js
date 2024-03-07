94092b1b00013e0a5c1dd8d963fed091d4fc94cc
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-03-07 12:27:17 +0100

    Feat: Adds babel-loader to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,18 @@ module.exports = {
   ], module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }]
+            ]
+          }
+        }
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
